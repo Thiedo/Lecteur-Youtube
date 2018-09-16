2c7851c8ef8d4854fb5463553ee0ed21bb862719
Marame <Marame@Mac-mini-de-Marame.local>
2018-09-17 00:35:22 +0100

    Ajout de la TableView et création du modèle

---
+++ Lecteur Youtube/TableauController.swift
@@ -0,0 +1,58 @@
+//
+//  TableauController.swift
+//  Lecteur Youtube
+//
+//  Created by Marame on 16/09/2018.
+//  Copyright © 2018 Marame. All rights reserved.
+//
+
+import UIKit
+
+class TableauController: UIViewController, UITableViewDelegate,UITableViewDataSource {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    var chansons = [Chanson]()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+     tableView.delegate = self
+     tableView.dataSource = self
+        
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return chansons.count
+    }
+   
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    
+    func ajouterChanson(){
+        
+        chansons = [Chanson]()
+        let dionne = Chanson(artiste: "Dionne Warwick", titre: "That's What Friends Are For", code: "HyTpu6BmE88")
+        chansons.append(dionne)
+         let prayer = Chanson(artiste: "Aretha Franklin", titre: "say a little prayer", code: "KtBbyglq37E")
+        chansons.append(prayer)
+         let think = Chanson(artiste: "Aretha Franklin", titre: "Think", code: "Vet6AHmq3_s")
+        chansons.append(think)
+         let respet = Chanson(artiste: "Aretha Franklin", titre: " Respect", code: "OD3WOKLTRyQ")
+        chansons.append(respet)
+         let soul = Chanson(artiste: "Tracy Chapman", titre: "Give Me One Reason", code: "V6hQ9HSKlIE")
+        chansons.append(soul)
+        let sha = Chanson(artiste: "Shania Twain", titre: "I Ain't No Quitter", code: "4G8bI17_AAc")
+        chansons.append(sha)
+         let shana = Chanson(artiste: "Shania Twain", titre: "Whose Bed Have Your Boots Been Under", code: "iZRA-Dwv86E")
+        chansons.append(shana)
+         let stev = Chanson(artiste: "Stevie Wonder", titre: "I Just Called To Say I Love You", code: "wFEH4GYXhAk")
+        chansons.append(stev)
+         let gloria = Chanson(artiste: "Gloria Gaynor", titre: "I Will Survive", code: "ARt9HV9T0w8")
+        chansons.append(gloria)
+        tableView.reloadData()
+    }
+}
